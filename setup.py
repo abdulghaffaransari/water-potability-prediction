285805b88dba4c6d00974ce2ed8f30893ca0b250
Abdul Ghaffar Ansari <88545317+abdulghaffaransari@users.noreply.github.com>
2024-09-25 18:41:09 +0200

    Added Cookie Cutter Template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Water portability Prediction using Machine Learning models',
+    author='Abdul-Ghaffar-Ansari',
+    license='',
+)
