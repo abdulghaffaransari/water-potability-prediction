a45ca31eae5e37badced49d38794e2adc016b473
Abdul Ghaffar Ansari <88545317+abdulghaffaransari@users.noreply.github.com>
2024-09-26 15:47:39 +0200

    Final Version of DVC exp

--- src/model/model_eval.py
+++ src/model/model_eval.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 import pickle
 import json
+from dvclive import Live
+import yaml
 
 from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
 
@@ -39,12 +41,26 @@ def load_model(filepath:str):
 
 def evaluation_model(model, X_test:pd.DataFrame, y_test:pd.Series) -> dict:
     try:
+        params = yaml.safe_load(open("params.yaml","r"))
+        
+        test_size = params["data_collection"]["test_size"]
+        n_estimators = params["model_building"]["n_estimators"]
+        
         y_pred = model.predict(X_test)
 
         acc = accuracy_score(y_test,y_pred)
         pre = precision_score(y_test,y_pred)
         recall = recall_score(y_test,y_pred)
         f1score = f1_score(y_test,y_pred)
+        
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric("Accuracy: ",acc)
+            live.log_metric("Precision: ",pre)
+            live.log_metric("recall: ",recall)
+            live.log_metric("f1score: ",f1score)
+            live.log_param("Test_Size: ",test_size)
+            live.log_param("n_estimators: ",n_estimators)
+            
 
         metrics_dict = {
 
